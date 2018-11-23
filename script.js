07fc8c99d6832f6ed520d9837de7ff7f11f157b1
ReedLikesBooks <40306894+ReedLikesBooks@users.noreply.github.com>
2018-11-23 11:51:45 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ var canvas = document.getElementById("myCanvas");
     var ctx = canvas.getContext("2d");
     if(2 < 3){
     ctx.beginPath();
-    ctx.arc(95, 50, 40, 0, 2 * Math.PI);
+    ctx.arc(95, 150, 40, 0, 2 * Math.PI);
     ctx.stroke();
     }
